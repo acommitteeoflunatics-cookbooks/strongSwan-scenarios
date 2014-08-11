36bb5ef219793a806ccdd15412789c32a661ae58
Jerry Jackson <jerry.jackson@acommitteeoflunatics.com>
2014-08-11 17:37:34 -0500

    adding a basic rakefile for TravisCI integration.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'strongSwan-scenarios'
 maintainer       'Jerry Jackson'
 maintainer_email 'jerry.jackson@acommitteeoflunatics.com'
-license          'Apache v2'
-description      'Installs/Configures strongSwan-scenarios'
+license          'Apache v2.0'
+description      'Provides usable scenario templates tested for strongSwan-base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 
